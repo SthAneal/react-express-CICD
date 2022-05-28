cda1d3634c1c2c62770be188081640ada25c4335
sthAneal <anilmeronaam@gmail.com>
2022-05-28 16:08:22 +1000

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+type userInfo = {
+  name:string
+}
+
+function App() {
+  const [user, setUser] = useState<userInfo>({} as userInfo);
+
+  useEffect(()=>{
+    getName();
+  }, [])
+
+  const getName = async ()=>{
+    const response = await axios.get('/name');
+    console.log(response);
+    setUser({name:response.data});
+  }
+
+  return (
+    <>
+      <div>User</div>
+      <div>{user.name}</div>
+    </>
+  );
+}
+
+export default App;
