0cfb544437d49f1068ae62cbc76734faf1b6a5c1
sthAneal <anilmeronaam@gmail.com>
2022-05-29 11:39:46 +1000

    react route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from 'axios';
 import './App.css';
+import {Login} from './components/Login';
+import {Register} from './components/Register';
+import {Home} from './components/Home';
+
 
 type userInfo = {
   name:string
@@ -23,6 +28,13 @@ function App() {
     <>
       <div>User name:</div>
       <div>{user.name}</div>
+      <Router>
+        <Routes>
+          <Route path="/login" caseSensitive={false} element={<Login />} />
+          <Route path="/register" caseSensitive={false} element={<Register />} />
+          <Route path="/" caseSensitive={false} element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
