d69b897aa6f59b8ecd444f66dcd843eec26fc34e
sthAneal <anilmeronaam@gmail.com>
2022-06-02 18:20:24 +1000

    axios api corrected

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/api/name', (req, res)=>{
     res.send('Anil Shrestha + Mira Shresthaa');
 });
 
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname, './frontend/build','index.html'));
+})
+
 app.listen(5000, ()=>{
     console.log('server is running on port 5000');
 });
